51b7201b785a719c6af13764cb0d3bef9719c9cc
Brijesh Sharma <brijesh.sharma1@publicissapient.com>
2020-09-08 12:50:39 +0530

    Initial Checkin

---
+++ src/os/aws/lambda/cal/ui/Test.java
@@ -0,0 +1,14 @@
+package os.aws.lambda.cal.ui;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
