df9973ceb2b5707dc74088e30fb659583b05ebbb
Brijesh Sharma <brijesh.sharma1@publicissapient.com>
2020-09-08 11:55:41 +0530

    First Check-in

---
+++ aws/lambda/calculator/ui/ConfigScreen.java
@@ -0,0 +1,57 @@
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author Brijesh Sharma
+ *
+ */
+public class ConfigScreen extends JFrame {
+
+	/**
+	 * @throws HeadlessException
+	 */
+	public ConfigScreen() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param gc
+	 */
+	public ConfigScreen(GraphicsConfiguration gc) {
+		super(gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param title
+	 * @throws HeadlessException
+	 */
+	public ConfigScreen(String title) throws HeadlessException {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param title
+	 * @param gc
+	 */
+	public ConfigScreen(String title, GraphicsConfiguration gc) {
+		super(title, gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ aws/lambda/calculator/ui/package-info.java
@@ -0,0 +1,1 @@
+package aws.lambda.calculator.ui;
