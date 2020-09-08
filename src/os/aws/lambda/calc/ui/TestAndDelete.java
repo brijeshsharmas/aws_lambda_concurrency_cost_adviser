c6355b92abaac569319c1066f0568490460935e0
Brijesh Sharma <brijesh.sharma1@publicissapient.com>
2020-09-08 12:28:08 +0530

    Initial Cleanup

---
+++ src/os/aws/lambda/calc/ui/TestAndDelete.java
@@ -0,0 +1,14 @@
+package os.aws.lambda.calc.ui;
+
+public class TestAndDelete {
+
+	public TestAndDelete() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
