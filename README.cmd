b24326980d43dc30f8e3aba313fb663627a8bc30
Brijesh Sharma <brijesh.sharma1@publicissapient.com>
2020-10-01 16:42:14 +0530

    Version 1.0

--- README.cmd
+++ README.cmd
@@ -30,4 +30,4 @@ Pull requests are welcome. For major changes, please open an issue first to disc
 Please make sure to update tests as appropriate.
 
 ## License
-[MIT](https://github.com/brijeshsharmas/aws_lambda_concurrency_cost_adviser/blob/master/LICENSE)
+(https://github.com/brijeshsharmas/aws_lambda_concurrency_cost_adviser/blob/master/LICENSE)
