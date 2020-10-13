e7271e8ef6fbda717b17bf8a136b63781356864d
Brijesh Sharma <brijesh.sharma1@publicissapient.com>
2020-10-13 10:33:36 +0530

    Version 1.0

--- src/os/aws/lambda/cal/LambdaCalculator.java
+++ src/os/aws/lambda/cal/LambdaCalculator.java
@@ -1,7 +1,7 @@
 /**
  * @author Brijesh Sharma
  * Copyright (c) 2020, Brijesh Sharma 
- * All rights reserved.
+ * All rights reserved. 
  * This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree. 
  */
 
